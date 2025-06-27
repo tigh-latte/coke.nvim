a05c2e11ef264b207d149db475902ce2cb223626
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2025-06-27 17:16:59 +0100

    make filepath relative for files under the current working directory

--- lua/coke/components/file.lua
+++ lua/coke/components/file.lua
@@ -1,11 +1,28 @@
 local M = {}
 
-function M:fmt()
-	return " %f%m "
+M.events = {
+	kind = "DirChanged",
+	opts = {
+		callback = function(ev)
+			M.cwd = ev.file
+		end,
+	},
+}
+
+function M:fmt(ctx)
+	local fname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(ctx.winnr))
+
+	if vim.startswith(fname, M.cwd) then
+		return " " .. fname:sub(#M.cwd + 2, #fname) .. "%m%r%h "
+	end
+
+	return " %f%m%r%h "
 end
 
 function M.colour()
 	return "%#CokeTransparent#"
 end
+
+M.events.opts.callback({ file = vim.uv.cwd() })
 
 return M

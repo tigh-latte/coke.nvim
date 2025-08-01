4a8384ec4a5672fe372c1b75f7a832617ba28ece
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2025-08-01 22:15:33 +0100

    truncate at filename

--- lua/coke/components/file.lua
+++ lua/coke/components/file.lua
@@ -13,10 +13,10 @@ function M:fmt(ctx)
 	local fname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(ctx.winnr))
 
 	if vim.startswith(fname, M.cwd) then
-		return " " .. fname:sub(#M.cwd + 2, #fname) .. "%m%r%h "
+		return " %<" .. fname:sub(#M.cwd + 2, #fname) .. "%m%r%h "
 	end
 
-	return " %f%m%r%h "
+	return " %<%f%m%r%h "
 end
 
 function M.colour()

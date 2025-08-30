79b443d9405f949e93b8ec2b63ce8fa3a7ffae43
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2025-08-30 21:01:22 +0100

    remove unneeded mode text from location

--- lua/coke/components/location.lua
+++ lua/coke/components/location.lua
@@ -1,25 +1,5 @@
-local M = {
-	mode_map = {
-		n = { fg = "#212121", bg = "#d7af87" },
-		niI = { bg = "#d7af87" },
-		no = { bg = "#d7af87" },
-		i = { fg = "#212121", bg = "#73b8f1" },
-		R = { bg = "#af5f5a" },
-		Rv = { bg = "#af5f5a" },
-		ic = { c = "C", bg = "#53892c" },
-		c = { bg = "#53892c" },
-		v = { bg = "#d19a66" },
-		V = { bg = "#d19a66" },
-		[""] = { fg = "#212121", bg = "#d19a66" },
-		s = { fg = "#212121", bg = "#d19a66" },
-		S = { bg = "#d19a66" },
-		nt = { bg = "#53892c" },
-		t = { bg = "#53892c" },
-	},
+return {
+	fmt = function()
+		return " %p%% %l/%L:%c "
+	end,
 }
-
-function M:fmt(ctx)
-	return " %p%% %l/%L:%c "
-end
-
-return M
